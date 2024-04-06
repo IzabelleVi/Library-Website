03ddef326899ddff2c45682001a17c3014f55d2b
Izabelle <74457757+IzabelleVi@users.noreply.github.com>
2024-04-06 21:09:08 +0200

    Create checklogin.js

---
+++ Routes/checklogin.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../Database/library.db');
+
+// route to bring a user to  login page if not logged in
+router.post('/profile', (req, res) => {
+if (!userId) {
+    window.location.href = '../Views/register.html';;
+    }
+});
